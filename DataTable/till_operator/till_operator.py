dbc1037f1d5814b8537ecbcd9a1f9de0b9c4a368
steve_n <ngwustephen99@gmail.com>
2020-02-12 13:27:06 +0100

    requirements and till operator update

--- DataTable/till_operator/till_operator.py
+++ DataTable/till_operator/till_operator.py
@@ -2,9 +2,9 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.uix.modalview import ModalView
 
-import unittest
-import math
 import re
 from pymongo import MongoClient
 
@@ -37,15 +37,18 @@ class OperatorWindow(BoxLayout):
                                 pos_hint={'top': 1})
             products_container.add_widget(details)
 
-            code = Label(text=pcode, size_hint_x=.3, color=(.06, .45, .45, 1))
+            code = Label(text=pcode, size_hint_x=.3,
+                         bold=True, color=(.06, .45, .45, 1))
             name = Label(text=target_code['product_name'],
                          size_hint_x=.3, color=(.06, .45, .45, 1))
             qty = Label(text=str(self.ids.qty_inp.text),
                         size_hint_x=.1, color=(.06, .45, .45, 1))
-            disc = Label(text='0.00', size_hint_x=.1, color=(.06, .45, .45, 1))
+            disc = Label(text=str(self.ids.disc_inp.text),
+                         size_hint_x=.1, color=(.06, .45, .45, 1))
             price = Label(
                 text=str(target_code['product_price']), size_hint_x=.1, color=(.06, .45, .45, 1))
-            total = Label(text=str(target_code['product_price']), size_hint_x=.2,
+
+            total = Label(text=str(float(price.text) * int(self.ids.qty_inp.text) - int(self.ids.disc_inp.text)), size_hint_x=.2,
                           color=(.06, .45, .45, 1))
             details.add_widget(code)
             details.add_widget(name)
@@ -58,6 +61,21 @@ class OperatorWindow(BoxLayout):
             pname = name.text
             pprice = float(price.text) * int(self.ids.qty_inp.text)
             pqty = str(self.ids.qty_inp.text)  # 1
+
+            # Discount implementation
+            try:
+                disc_var = self.ids.disc_inp.text
+                if disc_var == None:
+                    pass  # disc_var = 0
+                else:
+                    discount = int(self.ids.disc_inp.text)
+                    if discount > 0:
+                        pprice -= discount
+                    else:
+                        pprice
+            except ValueError:
+                print("something just went wrong")
+
             self.total += pprice
 
             purchase_total = '`\n\nTotal\t\t\t\t\t\t\t\t\t\t\t' + \
@@ -70,27 +88,28 @@ class OperatorWindow(BoxLayout):
             if _prev > 0:
                 prev_text = prev_text[:_prev]
 
-            ptarget = -1
-            for i, c in enumerate(self.cart):
-                if c == pcode:
-                    ptarget = i
+            # ptarget = -1
+            # for i, c in enumerate(self.cart):
+            #     if c == pcode:
+            #         ptarget = i
 
-            if ptarget >= 0:
-                pqty = self.qty[ptarget]  # self.ids.qty_inp.text
-                self.ids.qty_inp.text = pqty  # self.qty[ptarget]
-                expr = '%s\t\tx\d\t' % (pname)
-                rexpr = pname+'\t\tx'+str(pqty)+'\t'
-                nu_text = re.sub(expr, rexpr, prev_text)
-                preview.text = nu_text + purchase_total
-            else:
-                self.cart.append(pcode)
-                self.qty.append(self.ids.qty_inp.text)  # 1
-                nu_preview = '\n'.join(
-                    [prev_text, pname+'\tx'+pqty+'\t'+str(price.text), purchase_total])
-                preview.text = nu_preview
+            # if ptarget >= 0:
+            #     pqty = self.qty[ptarget]  # self.ids.qty_inp.text
+            #     self.ids.qty_inp.text = pqty  # self.qty[ptarget]
+            #     expr = '%s\t\tx\d\t' % (pname)
+            #     rexpr = pname+'\t\tx'+str(pqty)+'\t'
+            #     nu_text = re.sub(expr, rexpr, prev_text)
+            #     preview.text = nu_text + purchase_total
+            # else:
+            self.cart.append(pcode)
+            self.qty.append(self.ids.qty_inp.text)  # 1
+            nu_preview = '\n'.join(
+                [prev_text, pname+'\tx'+pqty+'\t'+str(price.text), purchase_total])
+            preview.text = nu_preview
+            print(self.cart)
+            print(self.qty)
 
-            self.ids.disc_inp.text = '0.00'
-            self.ids.disc_perc_inp.text = '0'
+            self.ids.disc_perc_inp.text = 'Hmm!'
             self.ids.qty_inp.text = str(pqty)
             self.ids.price_inp.text = str(price.text)
             self.ids.vat_inp.text = '15%'
@@ -101,9 +120,15 @@ class OperatorWindow(BoxLayout):
         # print(target_stock)
 
     def sold(self):
-        content = self.ids.code_inp
-        content.clear_widgets()
-        self.ids.refresh
+        pcode = self.ids.code_inp.text
+        target_code = self.stocks.find_one({'product_code': pcode})
+        print(target_code['sold'])
+        target_code['sold'] += int(self.ids.qty_inp.text)
+        print(target_code['sold'])
+        print(target_code)
+        # content = self.ids.code_inp
+        # content.clear_widgets()
+        # self.ids.refresh
 
 
 class OperatorApp(App):
